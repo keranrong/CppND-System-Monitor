9e58471a7309ccdee1cf794d2f10bc0165fd4057
keranrong <keranrong@gmail.com>
2020-09-18 22:09:25 -0400

    complete the Format.cpp, tested in seprately

--- src/format.cpp
+++ src/format.cpp
@@ -2,10 +2,25 @@
 
 #include "format.h"
 
+
 using std::string;
 
-// TODO: Complete this helper function
+// Reframe the time to double digit number string
+// INPUT: int unit time in sec, min or hours
+// OUTPUT: double digit number string
+string Format::TimeFormat(int time) {
+  if (time < 10) return "0" + std::to_string(time);
+  return std::to_string(time);
+}
+
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+  int sec = seconds % 60;
+  seconds -= sec;
+  int min = seconds % 3600 / 60;
+  seconds -= min * 60;
+  int hr = seconds / 3600;
+  return string(Format::TimeFormat(hr) + ':' + Format::TimeFormat(min) + ':' +
+                Format::TimeFormat(sec));
+}
