04b80faf86987d85ad1919fce5bf34c1ba3f38d0
keranrong <keranrong@gmail.com>
2020-09-20 19:54:04 -0400

    fill process file untested

--- src/process.cpp
+++ src/process.cpp
@@ -1,33 +1,44 @@
+#include "process.h"
+
 #include <unistd.h>
+
 #include <cctype>
 #include <sstream>
 #include <string>
 #include <vector>
 
-#include "process.h"
-
+#include "linux_parser.h"
 using std::string;
 using std::to_string;
 using std::vector;
 
-// TODO: Return this process's ID
-int Process::Pid() { return 0; }
+// Untested: Return this process's ID
+int Process::Pid() const { return pidid_; }
 
-// TODO: Return this process's CPU utilization
-float Process::CpuUtilization() { return 0; }
+// Untested: Return this process's CPU utilization
+float Process::CpuUtilization() const {
+  long prev_actjif, prev_jif, actjif, jif;
+  prev_actjif = LinuxParser::ActiveJiffies(pidid_);
+  prev_jif = LinuxParser::Jiffies();
+  usleep(50000);  // invertal for calculate the instantaneous cpu util
+  actjif = LinuxParser::ActiveJiffies(pidid_);
+  jif = LinuxParser::Jiffies();
+  return 1.0 * (actjif - prev_actjif) / (jif - prev_jif);
+}
 
-// TODO: Return the command that generated this process
-string Process::Command() { return string(); }
+// Untested: Return the command that generated this process
+string Process::Command() const { return LinuxParser::Command(pidid_); }
 
-// TODO: Return this process's memory utilization
-string Process::Ram() { return string(); }
+// Untested: Return this process's memory utilization
+string Process::Ram() const{ return to_string(LinuxParser::Ram(pidid_));}
 
-// TODO: Return the user (name) that generated this process
-string Process::User() { return string(); }
+// Untested: Return the user (name) that generated this process
+string Process::User() const { return LinuxParser::User(pidid_); }
 
-// TODO: Return the age of this process (in seconds)
-long int Process::UpTime() { return 0; }
+// Untested: Return the age of this process (in seconds)
+long int Process::UpTime() const { return LinuxParser::UpTime(pidid_); }
 
-// TODO: Overload the "less than" comparison operator for Process objects
-// REMOVE: [[maybe_unused]] once you define the function
-bool Process::operator<(Process const& a[[maybe_unused]]) const { return true; }
+// Untested: Overload the "less than" comparison operator for Process objects
+bool Process::operator<(Process const& a) const {
+  return this->CpuUtilization() > a.CpuUtilization();
+}
